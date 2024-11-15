fa474e91e2619d2f6988a109a8b04a3baed561a9
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2024-11-15 14:43:58 +0100

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-violet-400 font-black">
+    <h1 className="text-3xl font-bold text-violet-400">
       Welcome to the world of Next.js
     </h1>
   );
