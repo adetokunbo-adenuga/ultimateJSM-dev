5dfb6be88ee856d640448af2055d57696fbf64b9
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2024-12-09 13:39:45 +0100

    Set up Light & Dark theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 const Home = () => (
   <>
-    <h1 className="h1-bold">Welcome to the world of Next.js(Inter)</h1>;
+    <div className="mr-2 mt-2 flex justify-end"></div>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
   </>
 );
 export default Home;
